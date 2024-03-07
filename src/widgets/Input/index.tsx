aee16f242a0405d3b92be5e6daf3c7dcfefe89fd
Daloniil <lepetinskaa@yahoo.com>
2024-03-07 15:19:00 +0200

    add eye for password

--- src/widgets/Input/index.tsx
+++ src/widgets/Input/index.tsx
@@ -1,55 +1,60 @@
-import React, { FC, useState } from 'react';
+import React, {FC, useState} from 'react';
 import cls from 'classnames';
 
 import classes from '../../styles/widgets/Input.module.scss';
 
-import eyeIcon from '../../assets/eye-show.svg';
+import eyeOpenIcon from '../../assets/eye-open.svg';
+import eyeCloceIcon from '../../assets/eye-close.svg';
+
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
-	error?: string;
-	label?: string;
+    error?: string;
+    label?: string;
 };
 
-export const Input: FC<InputProps> = ({ value, type, onChange, label, ...props }) => {
-	const [inputType, setInputType] = useState(type);
+export const Input: FC<InputProps> = ({value, type, onChange, label, ...props}) => {
+    const [inputType, setInputType] = useState(type);
+    const [eyeStatus, setEyeStatus] = useState(false);
 
-	const togglePassword = () => {
-		setInputType((prev) => (prev === 'password' ? 'text' : 'password'));
-	};
 
-	return (
-		<div className={classes['container']}>
-			{label && (
-				<label
-					htmlFor={props.id || props.name}
-					className={classes['input__label']}
-				>
-					{label}
-				</label>
-			)}
-			<input
-				className={cls(classes['input'], {
-					[classes['input--error']]: props.error,
-				})}
-				id={props.id || props.name}
-				value={value}
-				onChange={onChange}
-				type={inputType}
-				{...props}
-			/>
-			{type === 'password' && (
-				<img
-					src={eyeIcon}
-					className={cls(classes['input__password-icon'], {
-						[classes['input__password-icon-with-label']]: label,
-					})}
-					onClick={togglePassword}
-				/>
-			)}
-			{props.error && (
-				<p className={classes['input__error-label']}>{props.error}</p>
-			)}
-		</div>
-	);
+    const togglePassword = () => {
+        setInputType((prev) => (prev === 'password' ? 'text' : 'password'));
+        setEyeStatus(!eyeStatus)
+    };
+
+    return (
+        <div className={classes['container']}>
+            {label && (
+                <label
+                    htmlFor={props.id || props.name}
+                    className={classes['input__label']}
+                >
+                    {label}
+                </label>
+            )}
+            <input
+                className={cls(classes['input'], {
+                    [classes['input--error']]: props.error,
+                })}
+                id={props.id || props.name}
+                value={value}
+                onChange={onChange}
+                type={inputType}
+                {...props}
+            />
+            {type === 'password' && (
+                <img
+                    src={eyeStatus ? eyeOpenIcon : eyeCloceIcon}
+                    className={cls(classes['input__password-icon'], {
+                        [classes['input__password-icon-with-label']]: label,
+                    })}
+                    onClick={togglePassword}
+                />
+            )}
+            {props.error && (
+                <p className={classes['input__error-label']}>{props.error}</p>
+            )}
+        </div>
+    );
 };
 
